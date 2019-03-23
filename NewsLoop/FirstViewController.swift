868647c932cf3c8fda8142c85d38e95abc9cc204
ianschoenrock <theapphideaway@gmail.com>
2019-03-22 20:45:11 -0400

    Initial Commit

---
+++ NewsLoop/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NewsLoop
+//
+//  Created by ian schoenrock on 3/22/19.
+//  Copyright © 2019 ian schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NewsLoop/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NewsLoop
+//
+//  Created by ian schoenrock on 3/22/19.
+//  Copyright © 2019 ian schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
